82e06ef44a776b8a777a603429852023a7080f1d
Linfeng Qian <thewawar@gmail.com>
2020-03-28 14:09:38 +0800

    Fix compile error

--- c/charge.c
+++ c/charge.c
@@ -9,21 +9,21 @@
 #define SCRIPT_SIZE 32768
 #define SIGNATURE_SIZE 65
 
-#define MAX_INPUTS 64;
-#define MAX_OUTPUTS 64;
-#define ERROR_TOO_MANY_INPUTS -51;
-#define ERROR_TOO_MANY_OUTPUTS -52;
+#define MAX_INPUTS 64
+#define MAX_OUTPUTS 64
+#define ERROR_TOO_MANY_INPUTS -51
+#define ERROR_TOO_MANY_OUTPUTS -52
 
 // 1  bytes for state
 // 20 bytes for muta address
 // 8  bytes for amount
 #define DATA_SIZE 29
-#define MUTA_ADDRESS_SIZE;
+#define MUTA_ADDRESS_SIZE 20
 
-#define STATE_FREE 0;     // free
-#define STATE_CHARGE 1;   // charge-proposed
-#define STATE_POOL 2;     // charged
-#define STATE_WITHDRAW 3; // withdraw-proposed
+#define STATE_FREE 0     // free
+#define STATE_CHARGE 1   // charge-proposed
+#define STATE_POOL 2     // charged
+#define STATE_WITHDRAW 3 // withdraw-proposed
 
 typedef struct {
     char state;
@@ -49,10 +49,10 @@ udt_t parse_cell_data(unsigned char *cell_data) {
     return item;
 }
 
-int load_udts(udt *items, uint64_t *total_coins, size_t *length, size_t source) {
+int load_udts(udt_t *items, uint64_t *total_coins, size_t *length, size_t source) {
     int ret;
     uint64_t len = 0;
-    udt current_udt;
+    udt_t current_udt;
     unsigned char cell_data[DATA_SIZE];
 
     size_t index = 0;
@@ -90,13 +90,13 @@ int load_udts(udt *items, uint64_t *total_coins, size_t *length, size_t source) 
 
 int main() {
     int ret;
-    uint64_t len = 0;
-    unsigned char temp[TEMP_SIZE];
-    unsigned char lock_bytes[SIGNATURE_SIZE];
-    unsigned char script[SCRIPT_SIZE];
+    /* uint64_t len = 0; */
+    /* unsigned char temp[TEMP_SIZE]; */
+    /* unsigned char lock_bytes[SIGNATURE_SIZE]; */
+    /* unsigned char script[SCRIPT_SIZE]; */
 
     // load inputs
-    udt input_udts[MAX_INPUTS];
+    udt_t input_udts[MAX_INPUTS];
     uint64_t input_coins = 0;
     size_t input_length;
     ret = load_udts(input_udts, &input_coins, &input_length, CKB_SOURCE_GROUP_INPUT);
@@ -105,7 +105,7 @@ int main() {
     }
 
     // load outputs
-    udt output_udts[MAX_OUTPUTS];
+    udt_t output_udts[MAX_OUTPUTS];
     uint64_t output_coins = 0;
     size_t output_length;
     ret = load_udts(output_udts, &output_coins, &output_length, CKB_SOURCE_GROUP_OUTPUT);
@@ -114,7 +114,7 @@ int main() {
     }
 
     if (input_coins != output_coins) {
-        if ((input_length === 0) && (output_length == 1)) {
+        if ((input_length == 0) && (output_length == 1)) {
             // FIXME: issue token
         }
     }
